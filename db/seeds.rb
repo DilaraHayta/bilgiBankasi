fc0606d07cad39caf47817d060ccdfa7b6c8bf4a
DilaraHayta <d.hayta1995@gmail.com>
2017-07-14 18:33:16 +0300

    Html sayfalar customer için düzenlendi.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+  Customer.create(name: 'Allianz', description: 'Allianz SE merkezi Almanyanın Münih kentinde bulunan dünyanın en büyük sigorta ve finansal yatırım şirketlerinden birisidir.')
+  Customer.create(name: 'YapıKredi Bankası', description: 'Yapı Kredi, 1944 yılında Türkiye de bireylerin daha uygun şartlarda konut sahibi olmalarını sağlamak amacıyla Kazım Taşkent tarafından kurulan Türkiye nin ilk özel bankasıdır.')
+  Customer.create(name: 'Dell', description: 'Dell, Teksas merkezli, Lenovo ve HP nin ardından dünyanın en büyük ikinci kişisel bilgisayar üreticisidir.')
