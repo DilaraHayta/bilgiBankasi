09d4b4fed7da2eac4e1bb8012da27a03fb6aa2f7
DilaraHayta <d.hayta1995@gmail.com>
2017-07-14 20:40:42 +0300

    Customer sayfalar düzenlendi.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#new'
+  root 'customers#index'
   resources :users
 end
