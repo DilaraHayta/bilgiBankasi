5f6319849026b430e07ebb11d980bb4a8a15d5c2
DilaraHayta <d.hayta1995@gmail.com>
2017-07-18 17:20:12 +0300

    Sign up sayfası son değişiklikler eklendi.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  resources :users
+  get 'users/new', to: redirect('/kaydol')
+  resources :users, except: :index
+  get '/kaydol', to: 'users#new', as: :register
+
+
   resources :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'customers#index'
+
+
 end
