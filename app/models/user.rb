04fc02228010cda85488cd07d567a87b345c1954
DilaraHayta <d.hayta1995@gmail.com>
2017-07-25 15:23:42 +0300

    Profil sayfası düzenlendi. Devices modeline ait pageler düzenlendi.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-  has_attached_file :avatar, styles: { medium: "30x30>", thumb: "10x10>" }
+  has_attached_file :avatar
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
 
